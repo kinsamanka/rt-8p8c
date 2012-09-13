2573eb8f0fbe74c7e9c3b26cda5b9d8aeac039c3
kinsamanka@gmail.com <kinsamanka@gmail.com@2e78a002-e69d-bf6f-b524-10a2f18fce04>
2012-09-13 12:55:37 +0000

    firmware upload

---
+++ trunk/firmware/hardware.h
@@ -0,0 +1,64 @@
+//    Copyright (C) 2012 GP Orcullo
+//    
+//    This file is part of rt-8p8c, an ethernet based interface for LinuxCNC.
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+//
+
+#ifndef __HARDWARE_H__
+#define __HARDWARE_H__
+
+#define SYS_FREQ                        (80000000ul)    // Hz
+#define GetSystemClock()		(SYS_FREQ)
+#define	GetPeripheralClock()		(GetSystemClock())
+#define	GetInstructionClock()		(GetSystemClock())
+
+#define LED0_TRIS			TRISFbits.TRISF3
+#define LED0_IO				LATFbits.LATF3
+
+#define DO0_TRIS			TRISDbits.TRISD0
+#define DO0_IO				LATDbits.LATD0
+#define DO0_IO_1			(LATDSET = _LATD_LATD0_MASK)
+#define DO0_IO_0			(LATDCLR = _LATD_LATD0_MASK)
+
+#define init_phy_rst()			(TRISCCLR = BIT_14)
+#define set_phy_rst()			(LATCSET = BIT_14)
+#define clr_phy_rst()			(LATCCLR = BIT_14)
+
+#define STEPHI_A			(LATGSET = BIT_7)
+#define STEPLO_A			(LATGCLR = BIT_7)
+#define DIR_HI_A			(LATGSET = BIT_6)
+#define DIR_LO_A			(LATGCLR = BIT_6)
+
+#define STEPHI_X			(LATDSET = BIT_5)
+#define STEPLO_X			(LATDCLR = BIT_5)
+#define DIR_HI_X			(LATDSET = BIT_4)
+#define DIR_LO_X			(LATDCLR = BIT_4)
+
+#define STEPHI_Y			(LATDSET = BIT_3)
+#define STEPLO_Y			(LATDCLR = BIT_3)
+#define DIR_HI_Y			(LATDSET = BIT_2)
+#define DIR_LO_Y			(LATDCLR = BIT_2)
+
+#define STEPHI_Z			(LATGSET = BIT_9)
+#define STEPLO_Z			(LATGCLR = BIT_9)
+#define DIR_HI_Z			(LATGSET = BIT_8)
+#define DIR_LO_Z			(LATGCLR = BIT_8)
+
+#define DIR_HI_PWM			(LATFSET = BIT_4)
+#define DIR_LO_PWM			(LATFCLR = BIT_4)
+
+
+#endif /* __HARDWARE_H__ */
---
+++ trunk/firmware/main.c
@@ -0,0 +1,249 @@
+//    Copyright (C) 2012 GP Orcullo
+//    
+//    This file is part of rt-8p8c, an ethernet based interface for LinuxCNC.
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+//
+
+#include <p32xxxx.h>
+#include <plib.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "timer.h"
+#include "uip.h"
+#include "uip_arp.h"
+#include "etherdev.h"
+#include "hardware.h"
+#include "stepgen.h"
+
+#pragma config \
+	FPLLODIV = DIV_1, \
+	FPLLMUL = MUL_20, \
+	FPLLIDIV = DIV_2, \
+	FWDTEN = OFF, \
+	FPBDIV = DIV_1,\
+	POSCMOD = XT, \
+	FNOSC = PRIPLL, \
+	CP = OFF, \
+	FSOSCEN = OFF, \
+	WDTPS = PS4096
+
+#pragma config \
+	FMIIEN = ON, \
+	FETHIO = ON	// external PHY in MII/normal configuration
+
+#define TOGGLES_PER_SEC			100000
+#define CORE_TICK_RATE	        	(SYS_FREQ/2/TOGGLES_PER_SEC)
+#define CORE_DIVIDER			(TOGGLES_PER_SEC/CLOCK_CONF_SECOND)
+
+#define ENABLE_WATCHDOG
+
+
+struct timer wdt_timer;
+static clock_time_t volatile timeval = 0;
+
+clock_time_t clock_time(void) {
+	return timeval;
+}
+
+#define UDPBUF ((struct uip_udpip_hdr *)&uip_buf[UIP_LLH_LEN])
+#define BUF    ((struct uip_eth_hdr *)&uip_buf[0])
+
+void init_io_ports()
+{
+	LED0_TRIS = 0;
+	LED0_IO = 0;
+
+	DO0_TRIS = 0;
+	DO0_IO = 0;
+	
+	// configure step and dir pins
+	// enable open collector
+	TRISDCLR = BIT_5 | BIT_4 | BIT_3 | BIT_2 | BIT_0;
+	ODCDSET = BIT_5 | BIT_4 | BIT_3 | BIT_2;
+	
+	TRISGCLR = BIT_7 | BIT_6 | BIT_9 | BIT_8;
+	ODCGSET = BIT_7 | BIT_6 | BIT_9 | BIT_8;
+
+	TRISFCLR = BIT_4;
+	ODCFSET = BIT_4;
+	
+	// TODO: configure 3.3v I/O pins
+}
+
+int main(void) {
+	int i;
+	uip_ipaddr_t ipaddr;
+	struct timer periodic_timer, arp_timer;
+	struct uip_udp_conn *c;
+
+	init_io_ports();
+
+	// Disable JTAG port so we get our I/O pins back
+	DDPCONbits.JTAGEN = 0;
+	// Enable optimal performance
+	SYSTEMConfigPerformance(GetSystemClock());
+	// Use 1:1 CPU Core:Peripheral clocks
+	OSCSetPBDIV(OSC_PB_DIV_1);
+
+	// configure the core timer roll-over rate (100msec)
+	OpenCoreTimer(CORE_TICK_RATE);
+
+	// set up the core timer interrupt with a priority of 7 
+	// and zero sub-priority
+	mConfigIntCoreTimer((CT_INT_ON | CT_INT_PRIOR_7 | CT_INT_SUB_PRIOR_0));
+
+	INTEnableSystemMultiVectoredInt();
+
+	timer_set(&periodic_timer, CLOCK_SECOND / 2);
+	timer_set(&arp_timer, CLOCK_SECOND * 10);
+	timer_set(&wdt_timer, CLOCK_SECOND );
+
+	ether_init();
+	uip_init();
+
+#if defined(ENABLE_WATCHDOG) 
+	WDTCONSET = 0x8000; 
+#endif 
+
+	uip_ipaddr(ipaddr, 10,19,74,25);
+//	uip_ipaddr(ipaddr, 10,0,0,2);
+	uip_sethostaddr(ipaddr);
+	uip_ipaddr(ipaddr, 255,255,255,0);
+	uip_setnetmask(ipaddr);
+
+	// setup UDP port to listen to
+	c = uip_udp_new(NULL, HTONS(0));
+	if (c != NULL) {
+		uip_udp_bind(c, HTONS(8888));
+	}
+
+	// main UIP control loop
+	while (1) {
+		uip_len = ether_read();
+		if (uip_len>0) {
+			if (BUF->type == htons(UIP_ETHTYPE_IP)) {
+				uip_arp_ipin();
+				uip_input();
+				/* If the above function invocation resulted in data that
+				should be sent out on the network, the global variable
+				uip_len is set to a value > 0. */
+				if (uip_len > 0) {
+					uip_arp_out();
+					ether_send();
+				}
+			} else if (BUF->type == htons(UIP_ETHTYPE_ARP)) {
+				uip_arp_arpin();
+				/* If the above function invocation resulted in data that
+				should be sent out on the network, the global variable
+				uip_len is set to a value > 0. */
+				if (uip_len > 0) {
+					ether_send();
+				}
+			}
+		} else if (timer_expired(&periodic_timer)) {
+
+			timer_reset(&periodic_timer);
+			LED0_IO ^= 1;
+
+			for (i=0; i<UIP_UDP_CONNS; i++) {
+				uip_udp_periodic(i);
+				/* If the above function invocation resulted in data that
+				should be sent out on the network, the global variable
+				uip_len is set to a value > 0. */
+				if (uip_len > 0) {
+					uip_arp_out();
+					ether_send();
+				}
+			}
+
+			/* Call the ARP timer function every 10 seconds. */
+			if (timer_expired(&arp_timer)) {
+				timer_reset(&arp_timer);
+				uip_arp_timer();
+			}
+		}
+		
+		// check if net interface is still alive
+		if (timer_expired(&wdt_timer)) {
+			stepgen_reset();
+		}
+
+#if defined(ENABLE_WATCHDOG) 
+		WDTCONSET = 0x01;
+#endif
+	}
+
+}
+
+// this procedure is called whenever UDP data is received
+void udp_appcall(void) {
+	u16_t len;
+	int32_t x;
+
+	// process received data
+	if (uip_newdata()) {
+		len = uip_datalen();
+		if (len > 3) {
+			memcpy((void*)&x,(const void*)uip_appdata,sizeof(x));
+						
+			switch (x) {
+				case 0x5453523E:	// >RST
+					stepgen_reset();
+					break;
+				case 0x444D433E:	// >CMD
+					// update
+					len = stepgen_update_input((const void*)uip_appdata+4);
+					// interface is alive
+					timer_restart(&wdt_timer);
+					break;
+				case 0x4746433E:	// >CFG
+					stepgen_update_config((const void*)uip_appdata+4);
+					break;
+			}
+			*((char *)uip_appdata) = '<';
+		}
+		uip_ipaddr_copy(&uip_udp_conn->ripaddr, &UDPBUF->srcipaddr);
+		uip_udp_conn->rport = UDPBUF->srcport;
+		uip_udp_send(len);
+
+	} else if (uip_poll()) {
+		// close remote port connection
+		uip_udp_conn->rport = 0;
+	}
+}
+
+// stepgen code is called every timer interrupt
+void __ISR(_CORE_TIMER_VECTOR, ipl7) CoreTimerHandler(void) {
+	static int count = CORE_DIVIDER;
+
+//	DO0_IO_1 ;
+	// update the period
+	UpdateCoreTimer(CORE_TICK_RATE);
+	
+	if (--count <= 0) {
+		timeval++;
+		count = CORE_DIVIDER;
+	}
+
+	stepgen();
+	
+	// clear the interrupt flag
+	mCTClearIntFlag();
+//	DO0_IO_0 ;
+}
+
